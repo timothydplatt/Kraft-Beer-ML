fb651e1f651bf83e1ab3ce9b241e643f1b9e942a
Timothy Platt <timothyplatt@timothys-mbp>
2018-08-21 14:10:20 +0100

    Initial Commit

---
+++ Kraft Beer ML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kraft Beer ML
+//
+//  Created by Timothy Platt on 21/08/2018.
+//  Copyright © 2018 Timothy Platt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
